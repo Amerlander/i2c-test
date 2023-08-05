6e56757529dde15af14716f7ba6eb5c131aaabb1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-08-05 16:44:51 +0200

    Demoprogramm 4 i2c Module, Lagesensor, Hardware-Sekundentakt an PIN0

--- main.ts
+++ main.ts
@@ -1,11 +1,8 @@
 function LCDZeile0 (row: number) {
     if (input.buttonIsPressed(Button.B)) {
-        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 0, 15, lcd16x2rgb.eAlign.right, bit.formatNumber(dipswitch.getBIN(), bit.eLength.BIN_11111111))
+        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 3, 11, lcd16x2rgb.eAlign.left, bit.formatNumber(dipswitch.getBIN(), bit.eLength.BIN_11111111))
     } else {
         lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 0, 11, lcd16x2rgb.eAlign.left, rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.ohne))
-    }
-    if (input.buttonIsPressed(Button.A)) {
-        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 12, 15, lcd16x2rgb.eAlign.left, "y " + input.rotation(Rotation.Roll))
     }
 }
 function Hintergrundfarbe () {
@@ -27,17 +24,16 @@ function Hintergrundfarbe () {
     lcd16x2rgb.setRGB(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5, rot, grün, blau)
 }
 function Speicherkarte () {
-    sName = "" + rtcpcf85063tp.getyyMMddHHmmss(0, 8) + ".LOG"
-    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 0, 0, 12, lcd16x2rgb.eAlign.left, sName)
-    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 1, 0, 8, lcd16x2rgb.eAlign.left, rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit))
-    qwiicopenlog.writeFile(qwiicopenlog.eADDR.LOG_Qwiic, sName, "" + sName + ";" + rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.mit) + ";" + rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit) + ";" + bit.formatNumber(dipswitch.getBIN(), bit.eLength.BIN_11111111) + ";" + input.temperature() + "°C;x:" + input.rotation(Rotation.Pitch) + ";y:" + input.rotation(Rotation.Roll) + ";A:" + input.buttonIsPressed(Button.A) + ";B:" + input.buttonIsPressed(Button.B), qwiicopenlog.eCRLF.CRLF)
-    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 0, 13, 15, lcd16x2rgb.eAlign.right, "Hallo" + "Welt")
+    Dateiname = "" + rtcpcf85063tp.getyyMMddHHmmss(0, 8) + ".LOG"
+    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 0, 0, 11, lcd16x2rgb.eAlign.left, Dateiname)
+    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 1, 3, 11, lcd16x2rgb.eAlign.left, rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit))
+    qwiicopenlog.writeFile(qwiicopenlog.eADDR.LOG_Qwiic, Dateiname, "" + Dateiname + ";" + rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.mit) + ";" + rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit) + ";" + bit.formatNumber(dipswitch.getBIN(), bit.eLength.BIN_11111111) + ";" + input.temperature() + "°C;x:" + DrehungX + ";y:" + DrehungY + ";A:" + input.buttonIsPressed(Button.A) + ";B:" + input.buttonIsPressed(Button.B), qwiicopenlog.eCRLF.CRLF)
 }
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    bLagesensor = true
+})
 function LCDZeile1 (row: number) {
     lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 3, 11, lcd16x2rgb.eAlign.left, rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit))
-    if (input.buttonIsPressed(Button.A)) {
-        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, row, 12, 15, lcd16x2rgb.eAlign.left, "x " + input.rotation(Rotation.Pitch))
-    }
 }
 function Binäruhr () {
     if (dipswitch.getON(dipswitch.eSwitch.DIP2, dipswitch.eONOFF.ON) && dipswitch.getON(dipswitch.eSwitch.DIP3, dipswitch.eONOFF.ON)) {
@@ -48,16 +44,19 @@ function Binäruhr () {
         basic.clearScreen()
     }
 }
-let sName = ""
-let blau = 0
-let grün = 0
-let rot = 0
-lcd16x2rgb.initRGB(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5)
-lcd16x2rgb.initLCD(lcd16x2rgb.eADDR_LCD.LCD_16x2)
-qwiicopenlog.checkStatusRegister(qwiicopenlog.eADDR.LOG_Qwiic)
-loops.everyInterval(1000, function () {
+pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
+    lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 1, 0, 0, lcd16x2rgb.eAlign.left, String.fromCharCode(bit.hex(bit.H4.xE0, bit.H0.x9)))
+    i2cCode()
+})
+function i2cCode () {
     rtcpcf85063tp.readDateTime(rtcpcf85063tp.eADDR.RTC_PCF85063TP)
     dipswitch.readSwitch(dipswitch.eADDR.DIP_SWITCH)
+    if (bLagesensor) {
+        DrehungX = input.rotation(Rotation.Pitch)
+        DrehungY = input.rotation(Rotation.Roll)
+        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 0, 12, 15, lcd16x2rgb.eAlign.right, bit.formatNumber(DrehungX, bit.eLength.toString))
+        lcd16x2rgb.writeText(lcd16x2rgb.eADDR_LCD.LCD_16x2, 1, 12, 15, lcd16x2rgb.eAlign.right, bit.formatNumber(DrehungY, bit.eLength.toString))
+    }
     Hintergrundfarbe()
     Binäruhr()
     if (dipswitch.getON(dipswitch.eSwitch.DIP1, dipswitch.eONOFF.OFF)) {
@@ -66,4 +65,18 @@ loops.everyInterval(1000, function () {
     } else {
         Speicherkarte()
     }
+}
+let DrehungY = 0
+let DrehungX = 0
+let Dateiname = ""
+let blau = 0
+let grün = 0
+let rot = 0
+let bLagesensor = false
+lcd16x2rgb.initRGB(lcd16x2rgb.eADDR_RGB.RGB_16x2_V5)
+lcd16x2rgb.initLCD(lcd16x2rgb.eADDR_LCD.LCD_16x2)
+qwiicopenlog.checkStatusRegister(qwiicopenlog.eADDR.LOG_Qwiic)
+bLagesensor = false
+loops.everyInterval(1000, function () {
+	
 })
